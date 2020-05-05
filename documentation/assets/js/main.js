3c1e2215f6290c40a213b4881ebcb497a59020b1
JackLu <jacklu0831@gmail.com>
2020-05-05 14:23:13 -0700

    wrote python script to html parser

--- documentation/assets/js/main.js
+++ documentation/assets/js/main.js
@@ -1,31 +1,49 @@
 $(document).ready(function() {
-	
-    /* ===== Stickyfill ===== */
-    /* Ref: https://github.com/wilddeer/stickyfill */
-    // Add browser support to position: sticky
     var elements = $('.sticky');
     Stickyfill.add(elements);
-
 
     /* Activate scrollspy menu */
     $('body').scrollspy({target: '#doc-menu', offset: 100});
     
     /* Smooth scrolling */
 	$('a.scrollto').on('click', function(e){
-        //store hash
         var target = this.hash;    
         e.preventDefault();
 		$('body').scrollTo(target, 200, {offset: 0, 'axis':'y'});
 		
 	});
-     
-    /* Bootstrap lightbox */
-    /* Ref: http://ashleydw.github.io/lightbox/ */
-
     $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
         e.preventDefault();
         $(this).ekkoLightbox();
     });    
-
-
 });
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /*loop through a collection of all HTML elements:*/
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /*make an HTTP request using the attribute value as the file name:*/
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /*remove the attribute, and call this function once more:*/
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /*exit the function:*/
+      return;
+    }
+  }
+};
+
+includeHTML();
